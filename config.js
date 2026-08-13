b066a5dfbb430dc746fd19afcdfd43d02681d54e
lissamphibiagirl <lissamphibia1@gmail.com>
2026-08-12 22:03:49 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-// ============================================================
-// EDIT THIS FILE AFTER YOU DEPLOY THE APPS SCRIPT (see SETUP.md)
-// ============================================================
-window.APP_CONFIG = {AKfycbyokFBvSky-m_nq4FozwHtFwh1Eq4TMvKcmhr75HbJWEXE7slKmSjUMd-CdnLYnPD76MQ
-  // Paste the Web App URL you get after deploying Code.gs
-  // It looks like: https://script.google.com/macros/s/AKfycb.../exec
-  APPS_SCRIPT_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
-
-  // How often (ms) to poll the server for other devices' updates while online
+window.APP_CONFIG = {
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxinQ4DR-LDxLoFoeER9e8wKnRd0YW0oi13f1ezU6UD0Jr6SagYIZEW2pKRclGiwqvJGg/exec",
   POLL_INTERVAL_MS: 20000
 };
