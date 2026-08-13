3cca7b6e9f2a75baa2173c8f75bc2c5883ad2c2c
lissamphibiagirl <lissamphibia1@gmail.com>
2026-08-12 21:50:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ============================================================
 // EDIT THIS FILE AFTER YOU DEPLOY THE APPS SCRIPT (see SETUP.md)
 // ============================================================
-window.APP_CONFIG = {
+window.APP_CONFIG = {AKfycbyokFBvSky-m_nq4FozwHtFwh1Eq4TMvKcmhr75HbJWEXE7slKmSjUMd-CdnLYnPD76MQ
   // Paste the Web App URL you get after deploying Code.gs
   // It looks like: https://script.google.com/macros/s/AKfycb.../exec
   APPS_SCRIPT_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
